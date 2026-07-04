83858d04de658ec3bf82e14ce160387429480eff
Subham <subhamsangwan26@gmail.com>
2026-07-04 07:27:03 +0530

    Update ESLint configuration for compatibility

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,7 @@ const compat = new FlatCompat({
   // import.meta.dirname is available after Node.js v20.11.0
   baseDirectory: import.meta.dirname,
 })
+
 const eslintConfig = [
   ...compat.config({
     extends: ['next'],
